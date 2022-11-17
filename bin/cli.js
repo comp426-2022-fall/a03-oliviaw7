e115c9a2e72f974fa43fcb94bc37be87aecaad44
Olivia Wen <oliviaw7@ad.unc.edu>
2022-11-17 16:02:49 -0500

    added default values for sides, dice, rolls

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,20 @@
 #!/usr/bin/env node 
 
-import minimist from "minimist";
+import minimist from "minimist"
 import { roll } from "../lib/roll.js"
  
 const args = minimist(process.argv.slice(2))
+
+if (!("sides" in args)) {
+    args.sides = 6
+}
+
+if (!("dice" in args)) {
+    args.dice = 2
+}
+
+if (!("rolls in args")) {
+    args.rolls = 1
+}
 
 console.log(JSON.stringify(roll(args.sides, args.dice, args.rolls)))
