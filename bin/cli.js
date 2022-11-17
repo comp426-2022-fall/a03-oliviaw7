490c70c918c322dfe5ea2ea83ddf8c66e056722f
Olivia Wen <oliviaw7@ad.unc.edu>
2022-11-17 15:53:13 -0500

    npm install

--- bin/cli.js
+++ bin/cli.js
@@ -5,8 +5,4 @@ import { roll } from "../lib/roll.js"
  
 const args = minimist(process.argv.slice(2))
 
-var sides = args.sides 
-var dice = args.dice 
-var rolls = args.rolls
-
-console.log(JSON.stringify(roll(sides, dice, rolls)))
+console.log(JSON.stringify(roll(args.sides, args.dice, args.rolls)))
